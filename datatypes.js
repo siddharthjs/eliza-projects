6306aec82f0ff7ce22bbb59048cbeead4a2025da
Siddharth Shekhar <510dharth.codes@gmail.com>
2023-05-17 12:36:46 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+let myName; // Variable declaration
+myName = "Eliza" // Variable initialization
+
+// Variable declaration and initializaton
+// String
+let yourName = "Sam"
+console.log(typeof yourName)
+
+// Number
+let age = 24
+console.log(typeof age)
+
+// Boolean
+let isRaining = true
+console.log(typeof isRaining)
+
+let willIBeGoing = false
+
+// Undefined
+let myFavFood;
+console.log(typeof myFavFood);
+
+// Null
+let myFavNum = null
+console.log(typeof myFavNum)
+
+// Symbol
---
+++ operators.js
@@ -0,0 +1,33 @@
+// Arthmetic operators -> Two operands
+// Addition operator
+let a = 10
+let b = 5
+
+let c = a + b
+console.log(c)
+
+// Subtraction operator
+console.log(a - b)
+
+// Modulus operator
+console.log(19%4)
+
+// Assignment opearators
+// Simple assignment operator
+let sum = a+b
+
+// Addition assignment operator
+a += 2
+
+// Subtraction assignment operator
+a -= 2
+
+// Multiplication assignment operator
+a *= 2
+
+// Division assignment operator
+a /= 2
+
+// Modulus assignment operator
+a %= 2
+
